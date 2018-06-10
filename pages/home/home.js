d250f6243bbe5dc25e20f08647cbb8dd617de0f0
isuperflying <1365961935@qq.com>
2018-06-10 11:51:13 +0800

    add answers

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,56 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    answers:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getData();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+  getData: function () {
+    var that = this;
+    wx.request({
+      url: 'http://127.0.0.1:8888/getfbdata',
+      method: 'GET',
+      data: {
+        'id': 1
+      },
+      success: function (res) {
+        wx.hideLoading();
+        console.log(res.data.data)
+        let answersArr = res.data.data.subject_options.split(' ');
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+        that.setData({
+          subject_title: res.data.data.subject_title,
+          answers: answersArr,
+        });
+      },
+      fail: function (res) {
+        wx.hideLoading();
+      }
+    })
   },
 
   /**
