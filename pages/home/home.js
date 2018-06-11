d997de9ba28de58d6e8606cf2810b5692d591c32
isuperflying <1365961935@qq.com>
2018-06-11 18:33:21 +0800

    add subjects

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,17 +1,28 @@
 // pages/home/home.js
-Page({
+var right_key
+var answerOrders = ['A','B','C','D']
+var rightIndex;
 
+var initNum = 8; //倒计时数
+var spaceNum = 1000; //文字倒计时间隔
+var space = 1000; //环倒计时间隔
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    answers:[]
+    answers:[],
+    answerColor: '',
+    clickIndex:'',
+    answerOver:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.countdown();
     this.getData();
   },
 
@@ -25,8 +36,16 @@ Page({
       },
       success: function (res) {
         wx.hideLoading();
-        console.log(res.data.data)
+        
         let answersArr = res.data.data.subject_options.split(' ');
+        right_key = res.data.data.right_key
+
+        answerOrders.forEach(function (obj,index) {
+            if(obj == right_key){
+              console.log(index)
+              rightIndex = index
+            }
+    　　});
 
         that.setData({
           subject_title: res.data.data.subject_title,
@@ -38,7 +57,89 @@ Page({
       }
     })
   },
+  answer:function(e){
+    let index = e.currentTarget.dataset.index
+    let userSelected = answerOrders[index]
+    if (userSelected == right_key) {//判断答案是否正确
+      console.log('--->yes')
+      this.setData({
+        clickIndex: e.currentTarget.dataset.index,
+        answerColor: 'right'
+      })
+    } else {
+      console.log('--->no')
+      this.setData({
+        clickIndex: e.currentTarget.dataset.index,
+        answerColor: 'error',
+        rightIndex : rightIndex
+      })
+    }
+    this.setData({
+      answerOver: ''
+    })
+    
+    let tizz = this
+    setTimeout(function(){
+      tizz.setData({
+        answerOver: 'shake'
+      })
+    }, 20)
+  },
 
+  begin: - (1 / 2 * Math.PI),
+  pai2: 2 * Math.PI,
+  //canvas画圆环
+  drawRang: function (precent) {
+    // 页面渲染完成  
+    var cxt_arc = wx.createCanvasContext('canvasArc');
+    var windowWidth = wx.getSystemInfoSync().windowWidth;
+    var poaitionX = wx.getSystemInfoSync().windowWidth / 2;
+    //var cxt_arc = this.cxt_arc;
+    cxt_arc.setLineWidth(10);
+    cxt_arc.setStrokeStyle('#279e50');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径   
+    cxt_arc.arc(poaitionX, 30, 24, 0, this.pai2, false);//设置一个原点(106,106)，半径为100的圆的路径到当前路径  
+    cxt_arc.stroke();//对当前路径进行描边  
+    var end = this.pai2 * precent + this.begin;
+    if (precent == 0) {
+      end = this.pai2 * + this.begin
+    } else if (1 == precent) {
+      cxt_arc.setFillStyle('white');
+      cxt_arc.setFontSize(25);
+      cxt_arc.fillText(this.data.countdownNum, poaitionX - 7, 40);
+      cxt_arc.draw();
+      return;
+    }
+    cxt_arc.setLineWidth(6);
+    cxt_arc.setStrokeStyle('#ffd84f');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径  
+    cxt_arc.arc(poaitionX, 30, 24, this.begin, end, true);
+    cxt_arc.stroke();//对当前路径进行描边  
+    cxt_arc.setFillStyle('#000');
+    cxt_arc.setFontSize(25);
+    cxt_arc.fillText(this.data.countdownNum, poaitionX - 7, 40);
+    cxt_arc.draw();
+  },
+  countdown: function () {
+    var that = this;
+    that.rangRun = 0;
+    clearInterval(that.cuntDownCir);
+    that.cuntDownCir = setInterval(function () {
+      var n = initNum - Math.floor(that.rangRun / spaceNum);
+      var precent = that.rangRun / (initNum * spaceNum);
+      that.setData({
+        countdownNum: n
+      });
+      that.drawRang(precent);
+      that.rangRun = that.rangRun + space;
+      if (precent >= 1) {
+        that.data.allowDo = false;
+        clearInterval(that.cuntDownCir);
+      }
+    }, space);
+  },
   /**
    * 用户点击右上角分享
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,53 +2,75 @@
 //获取应用实例
 const app = getApp()
 
+var initNum = 7; //倒计时数
+var spaceNum = 1000; //文字倒计时间隔
+var space = 1000; //环倒计时间隔
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
-  //事件处理函数
-  bindViewTap: function() {
+  onLoad:function(){
+    this.countdown();
+  },
+  start:function(){
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../home/home',
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  
+  begin: - (1 / 2 * Math.PI),
+  pai2: 2 * Math.PI,
+  //canvas画圆环
+  drawRang: function (precent) {
+    // 页面渲染完成  
+    var cxt_arc = wx.createCanvasContext('canvasArc');
+    var windowWidth = wx.getSystemInfoSync().windowWidth;
+    var poaitionX = wx.getSystemInfoSync().windowWidth / 2;
+    //var cxt_arc = this.cxt_arc;
+    cxt_arc.setLineWidth(10);
+    cxt_arc.setStrokeStyle('#ffffff');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径   
+    cxt_arc.arc(poaitionX, 30, 24, 0, this.pai2, false);//设置一个原点(106,106)，半径为100的圆的路径到当前路径  
+    cxt_arc.stroke();//对当前路径进行描边  
+    var end = this.pai2 * precent + this.begin;
+    if (precent == 0) {
+      end = this.pai2 * + this.begin
+    } else if (1 == precent) {
+      cxt_arc.setFillStyle('white');
+      cxt_arc.setFontSize(25);
+      cxt_arc.fillText(this.data.countdownNum, poaitionX - 7, 40);
+      cxt_arc.draw();
+      return;
     }
+    cxt_arc.setLineWidth(6);
+    cxt_arc.setStrokeStyle('#F15A47');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径  
+    cxt_arc.arc(poaitionX, 30, 24, this.begin, end, true);
+    cxt_arc.stroke();//对当前路径进行描边  
+    cxt_arc.setFillStyle('white');
+    cxt_arc.setFontSize(25);
+    cxt_arc.fillText(this.data.countdownNum, poaitionX - 7, 40);
+    cxt_arc.draw();
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  countdown: function () {
+    var that = this;
+    that.rangRun = 0;
+    clearInterval(that.cuntDownCir);
+    that.cuntDownCir = setInterval(function () {
+      var n = initNum - Math.floor(that.rangRun / spaceNum);
+      var precent = that.rangRun / (initNum * spaceNum);
+      that.setData({
+        countdownNum: n
+      });
+      that.drawRang(precent);
+      that.rangRun = that.rangRun + space;
+      if (precent >= 1) {
+        that.data.allowDo = false;
+        clearInterval(that.cuntDownCir);
+      }
+    }, space);
   }
 })
